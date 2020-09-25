899f59a9d206729bf8e38eb1b38937e27e7a19de
Clare Forrest <clare.a.forrest@gmail.com>
2020-09-25 14:40:33 +1000

    added loop to enter_user_data method

--- lib/comparison_app.rb
+++ lib/comparison_app.rb
@@ -7,7 +7,7 @@ class Test
   def initialize(name)
     @name = name
     @bank_statement
-    @payslips = payslips
+    @payslips=[]
     @compare_outcome = []
   end 
   #need to expand on the below so that the smartercsv.process becomes an actual method 
@@ -15,25 +15,43 @@ class Test
     @bank_statement = SmarterCSV.process('test_file.csv')
   end 
 
-  def enter_user_data
-    puts "please enter your first payslip"
-    p "> "
-    p payslips=[] << gets.chomp
+  def enter_user_data 
+    loop do
+      if @payslips[3] == nil
+        puts "please enter your payslip"
+        p "> "
+        @payslips << gets.chomp
+      else
+        break
+      end
+    end
   end 
+  
+  # def compare(bank_file, user_file)
+  #   bank_file.each do |line|
+  #   p line
+  #     end
+  #   user_file.each do |number|
+  #     p number
+  #     end 
+  #   end 
+  # def compare(bank_file, user_file)
+  #   bank_file.each do |key, value|
+  #     p key.include? user_file[0]
+  #   end
 
 end
 
+# arr=[1,2,4,3,4]
+# example=[3,4,3,2,1,3,4]
 
 sept = Test.new("september")
 sept.import_csv
 sept.bank_statement
 sept.enter_user_data
-sept.payslips
-# def enter_user_data(argv)
-#   p ARGF.read
-# end 
-  # def compare()
-  # end 
+# sept.payslips
+# sept.compare(sept.bank_statement, sept.payslips)
+
 
   # def display_comparison()
   # end 
