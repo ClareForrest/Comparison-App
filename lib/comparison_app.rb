231993b0dca11a05f6ef578cc8da8899d6bddead
Clare Forrest <clare.a.forrest@gmail.com>
2020-09-28 13:35:16 +1000

    added in new loop for compare_method

--- lib/comparison_app.rb
+++ lib/comparison_app.rb
@@ -1,28 +1,35 @@
-# require_relative "run"
-require 'smarter_csv'
+require_relative "run"
 
-class Test
+class Comparison_app
   #update attrs to be either read or write, not necessarily both
-  attr_accessor :name, :bank_statement, :payslips
+  attr_accessor :name, :bank_statement, :payslips, :orphan_bank_statements, :orphan_payslips
 
   def initialize(name)
     @name = name
     @bank_statement
     @payslips=[]
-    @compare_outcome = []
+    @orphan_bank_statements = orphan_bank_statements
+    @orphan_payslips = orphan_payslips
   end 
   #need to expand on the below so that the smartercsv.process becomes an actual method 
+  #ask user to advise on where csv file is stored. Pass this to the .process method 
+  def menu_selection
+  end 
+  
   def import_csv
-    @bank_statement = SmarterCSV.process('test_file.csv')
+    @bank_statement = SmarterCSV.process('bank_statement.csv')
   end 
 
   def enter_user_data 
     loop do
       #change the @payslips[index] back to 3
+      #or maybe use the number of items in the bank_statement to advise on number of entries
       if @payslips[1] == nil
         puts "please enter your payslip date"
         p "> "
         date = gets.chomp
+        puts "please enter your paysip amount"
+        p "> "
         amount = gets.chomp
         date_hash={:date => date, :amount => amount}
         @payslips << date_hash
@@ -32,31 +39,41 @@ class Test
     end
   end 
 
-    def compare_method
-      i = 0
-      loop do
+  def compare_method
+    orphan_bank_statements=[]
+    orphan_payslips=[]
+    if @bank_statement.eql? @payslips
+      puts "Data 1 matches Data 2"
+    else 
+      i=0
+      loop do 
         while i < @bank_statement.length
-          i = i + 1  
-          if @bank_statement[i][:date] == @payslips[i][:date]
-            puts "you have a match"
-          else 
-            puts "no match"
-            i += i+1
-          end
+          @bank_statement[i] 
+            @payslips.each do |pdata|
+              if @bank_statement[i] == pdata
+                puts "bank and payslips match".colorize(:green)
+                next 
+              else 
+                puts "no match".colorize(:red)
+                orphan_bank_statements << bank_statement[i]
+                orphan_payslips << pdata 
+              end 
+              i += 1
+            end 
+            break 
+          end 
         end 
-      end 
     end
+  end 
    
-    
-  
-  #I want to compare the key values from one hash against the key values of the second hash
-  # def compare(bank_data, user_data)
-    
-  # end 
-  
-end
+  def display_data
 
-sept = Test.new("september")
+  end 
+    
+end 
+  
+
+sept = Comparison_app.new("september")
 sept.import_csv
 sept.bank_statement
 sept.enter_user_data
