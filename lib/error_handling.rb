b2793c94486d7ec177306e451556d3da455cdc7a
Clare Forrest <clare.a.forrest@gmail.com>
2020-10-02 09:57:53 +1000

    ran rubocop and updated errors

--- lib/error_handling.rb
+++ lib/error_handling.rb
@@ -1,5 +1,28 @@
-# Should this be a class or a module?
+@bank_statement = [{ date: 1120, amount: 1234 }, { date: 2220, amount: 5678 }, { date: 3320, amount: 91_011 }]
+@payslips = []
 
-class FixingMistakes
+def enter_user_data
+    i = @bank_statement.length
+    loop do
+      if @payslips[i].nil?
+        begin
+          puts 'Please enter your payslip date'
+          print '> '
+          date = gets.chomp
+          puts 'Please enter your payslip amount'
+          print '> '
+          amount = gets.chomp
+          raise('Invalid entry. Please re-enter date and amount') if date == '' || amount == ''
+        rescue StandardError => e
+          p e
+          retry
+        end
+        date_hash = { date: date, amount: amount }
+        @payslips << date_hash
+      else
+        break
+      end
+    end
+  end
 
-end
+  enter_user_data
--- lib/run.rb
+++ lib/run.rb
@@ -4,13 +4,11 @@ require 'colorize'
 require 'tty-table'
 require 'tty-prompt'
 require 'smarter_csv'
-require 'rspec'
 require 'set'
 require 'optparse'
 
 require_relative 'help_menu'
 require_relative 'comparison_app'
 
-name = ComparisonApp.new("name")
-table = TTY::Table.new
+name = ComparisonApp.new('name')
 name.router
