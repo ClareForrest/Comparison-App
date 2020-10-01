e6235aa1e8b5548e0bed17a1e43e281f61075832
Clare Forrest <clare.a.forrest@gmail.com>
2020-10-02 09:56:59 +1000

    refactored enter_user_data method

--- lib/comparison_app.rb
+++ lib/comparison_app.rb
@@ -10,9 +10,8 @@ class ComparisonApp
     @bank_statement = import_csv
     @payslips = []
     @orphan_bank_statements = []
-    @orphan_payslips = orphan_payslips = []
+    @orphan_payslips = []
   end
-
 
   def menu_selection
     prompt = TTY::Prompt.new
@@ -43,42 +42,29 @@ class ComparisonApp
     end
   end
 
-  # need to expand on the below so that the smartercsv.process becomes an actual method
-  # ask user to advise on where csv file is stored. Pass this to the .process method
   def import_csv
-    # puts "Please enter the file path of your stored csv file"
-      # user_entry = gets.chomp
-      SmarterCSV.process('/Users/clareforrest/Desktop/bs_desktop.csv')
-    # raise Errno::ENOENT
-    #   puts "Please upload a bank statement for comparison in CSV format".colorize(:red)
+    SmarterCSV.process('bank_statement.csv')
+  rescue Errno::ENOENT
+    puts 'Please upload a bank statement for comparison in CSV format'.colorize(:red)
+    retry
   end
 
   def enter_user_data
     i = @bank_statement.length
     loop do
       if @payslips[i].nil?
-        puts 'Please enter your payslip date'
-        print '> '
-        date = gets.chomp
-        loop do
-          if date == ""
-            puts "Invalid entry. Please enter your payslip date"
-            date = gets.chomp
-          else 
-            break
-          end 
-        end 
-        puts 'Please enter your payslip amount'
-        print '> '
-        amount = gets.chomp
-        loop do
-          if amount == ""
-            puts "Invalid entry. Please enter payslip amout"
-            amount = gets.chomp
-          else 
-            break
-          end 
-        end 
+        begin
+          puts 'Please enter your payslip date'
+          print '> '
+          date = gets.chomp
+          puts 'Please enter your payslip amount'
+          print '> '
+          amount = gets.chomp
+          raise('Invalid entry. Please re-enter date and amount') if date == '' || amount == ''
+        rescue StandardError => e
+          p e
+          retry
+        end
         date_hash = { date: date, amount: amount }
         @payslips << date_hash
       else
@@ -90,11 +76,11 @@ class ComparisonApp
   def compare_method
     if @payslips.length.zero?
       puts 'Payslips file empty - Cannot compare'
-      self.enter_user_data
+      enter_user_data
       return
     end
     if @bank_statement.eql? @payslips
-      puts '#{@name.capitalize} comparison is complete - there are no discrepancies'
+      puts "#{@name.capitalize} comparison is complete - there are no discrepancies"
     else
       i = 0
       loop do
@@ -122,10 +108,10 @@ class ComparisonApp
     if @orphan_bank_statements.length > 1
       row1 = @orphan_bank_statements.map do |statements|
         [statements[:date], statements[:amount]]
-      end 
-        row2 = @orphan_payslips.map do |statements|
-          [statements[:date], statements[:amount]]
-        end
+      end
+      row2 = @orphan_payslips.map do |statements|
+        [statements[:date], statements[:amount]]
+      end
       @table = TTY::Table.new(rows: [row1, row2])
       puts "These are the dates and amounts that don't have a match".colorize(:yellow)
       puts @table.render(:ascii)
